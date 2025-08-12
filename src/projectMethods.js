98055a1b1796f5553d936c059ced0ed6baeba68d
williams-derek-j <williams-derek-j@proton.me>
2025-08-12 14:49:41 -0700

    added untracked files

---
+++ src/projectMethods.js
@@ -0,0 +1,47 @@
+import Task from "./task";
+
+export const projectMethods = {
+    toJSONString: function() {
+        const nonCircular = {};
+
+        for (let key in this) {
+            if (key !== 'render' && key !== 'tasks') {
+                if (typeof this[key] !== 'object' && typeof this[key] !== 'function') {
+                    nonCircular[key] = `${this[key]}`;
+                }
+            } else if (key === 'tasks') {
+                nonCircular['tasks'] = [];
+                this[key].forEach((task) => {
+                    nonCircular['tasks'].push(task.toJSONString());
+                })
+                nonCircular['tasks'] = JSON.stringify(nonCircular['tasks']);
+            }
+        }
+        console.log('nonCircProj:', nonCircular);
+        return JSON.stringify(nonCircular);
+    },
+
+    setRender: function(render) {
+        this.render = render;
+    },
+
+    createTask: function(user, props) {
+        let task = new Task(user, this, props);
+
+        task.index = this.tasks.length;
+
+        this.tasks.push(task);
+
+        return(task);
+    },
+
+    deleteTask: function(deleted) {
+        this.tasks = this.tasks.filter((task) => {
+            return task !== deleted;
+        })
+    },
+
+    getAllTasks: function() {
+        return this.tasks;
+    },
+}
---
+++ src/userMethods.js
@@ -0,0 +1,22 @@
+export const userMethods = {
+    toJSONString: function() {
+        const nonCircular = {};
+
+        for (let key in this) {
+            if (key !== 'render' && key !== 'projects') {
+                if (typeof this[key] !== 'object' && typeof this[key] !== 'function') {
+                    nonCircular[key] = `${this[key]}`;
+                }
+            } else if (key === 'projects') {
+                nonCircular['projects'] = [];
+
+                this[key].forEach((project) => {
+                    nonCircular['projects'].push(project.toJSONString());
+                })
+                console.log('nonCircularUser', nonCircular);
+                nonCircular['projects'] = JSON.stringify(nonCircular['projects']);
+            }
+        }
+        return JSON.stringify(nonCircular);
+    }
+}
